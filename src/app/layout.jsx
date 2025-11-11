a18c7c2a8a2d83096402fb4f598430b0e4037527
Amir Ali Barghamadi <amiralibarghamadi851@gmail.com>
2025-11-12 00:56:03 +0330

    add Lego and Fonts to the Project and Creating a Navbar

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,5 @@
+import localFont from "next/font/local";
+import Navbar from "@/components/Navbar/navbar";
 import "./globals.css";
 
 export const metadata = {
@@ -5,10 +7,19 @@ export const metadata = {
   description: "اسکای مووی مرجع دانلود فیلم سریال  بدون سانسور",
 };
 
+const myFont = localFont({
+  src: "../../public/font/SFArabic-Regular.woff2",
+});
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className="">
+    <html lang="fa" suppressHydrationWarning>
+      <body
+        className={`${myFont.className} `}
+        dir="rtl"
+        suppressHydrationWarning
+      >
+        <Navbar />
         {children}
       </body>
     </html>
---
+++ src/components/Navbar/navbar.jsx
@@ -0,0 +1,74 @@
+import Image from "next/image";
+import Logo from "../../../public/img/logo.png";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <>
+      <div className="bg-sky-400 dark:bg-sky-800 py-8 rounded-br-full">
+        <header className="container flex justify-between items-center">
+          <div className="flex items-center">
+            <Link href="/" className="flex items-center">
+              <div>
+                <Image
+                  src={Logo}
+                  alt="اسکای مووی"
+                  width={90}
+                  height={90}
+                  loading="lazy"
+                />
+              </div>
+              <div className="w-full items-center hidden sm:block">
+                <h1 className="text-lg md:text-xl text-sky-600">اسکای مووی</h1>
+              </div>
+            </Link>
+          </div>
+
+          <div className="hidden md:block">
+            <div>
+              <ul className="flex justify-between text-blak dark:text-black  items-center w-52">
+                <li>
+                  <Link
+                    href="/movies"
+                    className="dark:hover:bg-gray-800 transition-all dark:hover:text-white p-2 rounded-lg  "
+                  >
+                    فیلم ها
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/series"
+                    className="dark:hover:bg-gray-800 transition-all dark:hover:text-white p-2 rounded-lg  "
+                  >
+                    سریال
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    href="/about-us"
+                    className="dark:hover:bg-gray-800 transition-all dark:hover:text-white p-2 rounded-lg  "
+                  >
+                    درباره ما
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="hidden md:flex">
+            <div className="flex justify-between items-center w-40">
+              
+              <div className="text-black w-40">
+                <button className="dark:hover:bg-gray-800 transition-all dark:hover:text-white px-4.5 py-2 rounded-lg ">
+                  ورود \ ثبت نام
+                </button>
+              </div>
+            </div>
+          </div>
+
+          
+        </header>
+      </div>
+    </>
+  );
+}
